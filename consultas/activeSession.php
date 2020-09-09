f8df3a2cd256bad1c5867594a383da09135c4e80
joamontesgi <joamontesgi@unal.edu.co>
2020-09-08 20:28:46 -0500

    asd

--- consultas/activeSession.php
+++ consultas/activeSession.php
@@ -1,26 +1,16 @@
 <?php
 
 echo session_id();
-
 session_start();
-// error_reporting(0);
+
 $varSeccion=$_SESSION['nombre_style'];
 
-
-
-
-
-
-//$cedula=$_SESSION['cedula'];
-
-
-
 if($varSeccion== null || $varSeccion==''){
-    // echo "Debe iniciar sesion";
+
     header("Location: index.php");
-    // die();
+
 }
-// echo $_SESSION['usuario'];
+
 
 
 ?>
